f06c7f73699260eff01240a062038a1c46328701
Josephadelin22 <j.boussamba@alustudent.com>
2025-03-02 10:22:29 -0800

    Create a new sentence

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Python is an interpreted, interactive, object-oriented programming "
+str2 = "language that combines remarkable power with very clear syntax"
+
+print(f"{str1} {str2}")
