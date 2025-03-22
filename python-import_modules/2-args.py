4ca211c500182f6fdf4e7ea278cd1ae54a8f2957
Josephadelin22 <j.boussamba@alustudent.com>
2025-03-22 06:04:03 -0700

    Python - import & modules

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("0 arguments. ")
+else:
+    print("{} argument{}".format(len(sys.argv) - 1, "s" if len(sys.argv) > 2 else ""))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i,sys.argv[i]))
