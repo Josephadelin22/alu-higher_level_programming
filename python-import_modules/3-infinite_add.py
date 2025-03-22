d3ff57f4d086155486ce7c5b05294df240e2fb3e
Josephadelin22 <j.boussamba@alustudent.com>
2025-03-22 06:45:11 -0700

    Python - import & modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 import sys
 
 
-total = 0
-for arg in sys.argv[1:]:
-    total += int(arg)
+if __name__ == "__main__"
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
 
-
-print(total)
+    print(total)
