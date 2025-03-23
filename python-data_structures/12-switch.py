599a5c6b4c7f2963cdfe1016c150a70a9e6bf929
Josephadelin22 <j.boussamba@alustudent.com>
2025-03-23 12:07:37 -0700

    Python - Data Structures: Lists, Tuples

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
+
 def uppercase_str(my_str):
     result = ""
     for char in my_str:
-        if 'a' <= char <= 'z':
-            result += chr(ord(char) - 32)
+        if 'a' <= char <= 'z': result += chr(ord(char) - 32)
         else:
             result += char
     print(result)
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=None):
     if my_list is None or not my_list:
-        return none
-    return max(my_list)
+        return None
+    max_num = my_list[0]
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+    return max_num
